fc18b2a756e05f4b80d940040e76d5955ca4b22b
rehmanjameel <abdulrehmancs17@gmail.com>
2023-02-09 19:28:12 +0500

    change theme of app

--- app/src/main/java/org/codebase/slideo/MainActivity.kt
+++ app/src/main/java/org/codebase/slideo/MainActivity.kt
@@ -2,10 +2,17 @@ package org.codebase.slideo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
+
+    var toolbar: androidx.appcompat.widget.Toolbar? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        toolbar = findViewById(R.id.toolbar)
+//        setSupportActionBar(toolbar)
     }
 }
