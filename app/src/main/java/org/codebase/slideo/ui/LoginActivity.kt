806ced2fd7278dea28d7d2fe141b78e96853a266
rehmanjameel <abdulrehmancs17@gmail.com>
2023-02-14 12:35:41 +0500

    add login page

---
+++ app/src/main/java/org/codebase/slideo/ui/LoginActivity.kt
@@ -0,0 +1,19 @@
+package org.codebase.slideo.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
+import org.codebase.slideo.MainActivity
+import org.codebase.slideo.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        loginButtonId.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+}
