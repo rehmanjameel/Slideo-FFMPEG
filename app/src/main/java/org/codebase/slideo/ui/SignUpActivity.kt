a367862104432afa6d34088db59013286a166e62
rehmanjameel <abdulrehmancs17@gmail.com>
2023-03-15 19:44:45 +0500

    design signup page, add project in firebase

---
+++ app/src/main/java/org/codebase/slideo/ui/SignUpActivity.kt
@@ -0,0 +1,14 @@
+package org.codebase.slideo.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.codebase.slideo.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+
+    }
+}
