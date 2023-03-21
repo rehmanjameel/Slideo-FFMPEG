16f9aa6858cf208ecb2d620b6e5f56a9201130f6
rehmanjameel <abdulrehmancs17@gmail.com>
2023-03-21 14:40:04 +0500

    login user

--- app/src/main/java/org/codebase/slideo/ui/LoginActivity.kt
+++ app/src/main/java/org/codebase/slideo/ui/LoginActivity.kt
@@ -5,14 +5,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
+import android.widget.Toast
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_login.view.*
 import org.codebase.slideo.MainActivity
 import org.codebase.slideo.R
 import org.codebase.slideo.utils.App
 import org.codebase.slideo.viewmodel.SplashScreenViewModel
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,8 +30,10 @@ class LoginActivity : AppCompatActivity() {
         window.statusBarColor = resources.getColor(R.color.top_bar)
         setContentView(R.layout.activity_login)
 
+        auth = Firebase.auth
+
         loginButtonId.setOnClickListener {
-            login()
+//            login()
         }
 
         openSignUpPage.setOnClickListener {
@@ -36,7 +45,45 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
-    private fun login() {
+    fun validateLoginFields() {
+        val loginEmail = loginEmailId.text.toString()
+        val loginPassword = loginPasswordId.text.toString().trim()
+
+        if (loginEmail.isEmpty() && loginPassword.isEmpty()) {
+            loginEmailId.error = "Email is required"
+            loginPasswordId.error = "Email is required"
+        } else if (loginEmail.isEmpty()) {
+            loginEmailId.error = "Email is required"
+        } else if (loginPassword.isEmpty()) {
+            loginPasswordId.error = "Password is required"
+        } else {
+            login(email = loginEmail, password = loginPassword)
+        }
+
+    }
+    private fun login(email: String, password: String) {
         App.saveLogin(true)
+
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) {
+                val user = auth.currentUser
+
+                val intent = Intent(this, ProfileActivity::class.java)
+                startActivity(intent)
+
+            } else {
+                Toast.makeText(baseContext, "Authentication failed.",
+                    Toast.LENGTH_SHORT).show()
+            }
+
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+
+        }
     }
 }
---
+++ app/src/main/java/org/codebase/slideo/ui/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package org.codebase.slideo.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.codebase.slideo.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
